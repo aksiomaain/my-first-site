0e55b3cbfeb985b764ff9bc73641ba191510bb96
aksiomaain <45222234+aksiomaain@users.noreply.github.com>
2024-11-19 00:52:14 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// This is a comment
-console.log("Hello, JavaScript!"); // Prints a message in the browser console
-
-// Change the text of an element
-document.addEventListener("DOMContentLoaded", function () {
-    const heading = document.getElementById("main-heading");
-    heading.textContent = "Welcome to JavaScript!";
-});
